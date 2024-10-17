f95243f8307445ed28a2366aecea65c95b732c48
yashkurkure <yash.kurkure1903@gmail.com>
2024-10-17 15:05:52 -0500

    boslow 95% ci

--- plot/app.py
+++ plot/app.py
@@ -3,7 +3,9 @@ from exp_theta_two_parts import \
     violin_cmp_2_exp_wait_v_node_count, \
     violin_cmp_2_exp_wait_v_walltime, \
     violin_cmp_2_exp_boslo_v_node_count, \
-    violin_cmp_2_exp_boslo_v_walltime
+    violin_cmp_2_exp_boslo_v_walltime, \
+    violin_cmp_2_exp_boslo_v_node_count_95, \
+    violin_cmp_2_exp_boslo_v_walltime_95
 from dash import Dash, html, dcc, Input, Output, callback
 
 
@@ -65,6 +67,22 @@ def update_output(value):
                 exp2c2 = '../data/cloudlab/exp_theta_two_parts/probable_user_1_0.5/cluster_2/Results/theta_2022.rst',
                 exp_1_name = "SGS-T",
                 exp_2_name = "Random"
+            ),
+            *violin_cmp_2_exp_boslo_v_node_count_95(
+                exp1c1 ='../data/cloudlab/exp_theta_two_parts/optimal_turnaround_1/cluster_1/Results/theta_2022.rst',
+                exp1c2 = '../data/cloudlab/exp_theta_two_parts/optimal_turnaround_1/cluster_2/Results/theta_2022.rst',
+                exp2c1 = '../data/cloudlab/exp_theta_two_parts/probable_user_1_0.5/cluster_1/Results/theta_2022.rst',
+                exp2c2 = '../data/cloudlab/exp_theta_two_parts/probable_user_1_0.5/cluster_2/Results/theta_2022.rst',
+                exp_1_name = "SGS-T",
+                exp_2_name = "Random"
+            ),
+            *violin_cmp_2_exp_boslo_v_walltime_95(
+                exp1c1 ='../data/cloudlab/exp_theta_two_parts/optimal_turnaround_1/cluster_1/Results/theta_2022.rst',
+                exp1c2 = '../data/cloudlab/exp_theta_two_parts/optimal_turnaround_1/cluster_2/Results/theta_2022.rst',
+                exp2c1 = '../data/cloudlab/exp_theta_two_parts/probable_user_1_0.5/cluster_1/Results/theta_2022.rst',
+                exp2c2 = '../data/cloudlab/exp_theta_two_parts/probable_user_1_0.5/cluster_2/Results/theta_2022.rst',
+                exp_1_name = "SGS-T",
+                exp_2_name = "Random"
             )
         ]),
         dcc.Tab(label='SGS-T vs Random (Heterogeneous)', children=[
@@ -93,6 +111,22 @@ def update_output(value):
                 exp_2_name = "Random"
             ),
             *violin_cmp_2_exp_boslo_v_walltime(
+                exp1c1 ='../data/cloudlab/exp_theta_two_parts/optimal_turnaround_1.3/cluster_1/Results/theta_2022.rst',
+                exp1c2 = '../data/cloudlab/exp_theta_two_parts/optimal_turnaround_1.3/cluster_2/Results/theta_2022.rst',
+                exp2c1 = '../data/cloudlab/exp_theta_two_parts/probable_user_1.3_0.5/cluster_1/Results/theta_2022.rst',
+                exp2c2 = '../data/cloudlab/exp_theta_two_parts/probable_user_1.3_0.5/cluster_2/Results/theta_2022.rst',
+                exp_1_name = "SGS-T",
+                exp_2_name = "Random"
+            ),
+            *violin_cmp_2_exp_boslo_v_node_count_95(
+                exp1c1 ='../data/cloudlab/exp_theta_two_parts/optimal_turnaround_1.3/cluster_1/Results/theta_2022.rst',
+                exp1c2 = '../data/cloudlab/exp_theta_two_parts/optimal_turnaround_1.3/cluster_2/Results/theta_2022.rst',
+                exp2c1 = '../data/cloudlab/exp_theta_two_parts/probable_user_1.3_0.5/cluster_1/Results/theta_2022.rst',
+                exp2c2 = '../data/cloudlab/exp_theta_two_parts/probable_user_1.3_0.5/cluster_2/Results/theta_2022.rst',
+                exp_1_name = "SGS-T",
+                exp_2_name = "Random"
+            ),
+            *violin_cmp_2_exp_boslo_v_walltime_95(
                 exp1c1 ='../data/cloudlab/exp_theta_two_parts/optimal_turnaround_1.3/cluster_1/Results/theta_2022.rst',
                 exp1c2 = '../data/cloudlab/exp_theta_two_parts/optimal_turnaround_1.3/cluster_2/Results/theta_2022.rst',
                 exp2c1 = '../data/cloudlab/exp_theta_two_parts/probable_user_1.3_0.5/cluster_1/Results/theta_2022.rst',
--- plot/exp_theta_two_parts.py
+++ plot/exp_theta_two_parts.py
@@ -76,7 +76,8 @@ def violin_cmp_2_exp_wait_v_node_count(
             name=exp_1_name,
             side='negative',
             line_color='blue',
-            showlegend=showlegend
+            showlegend=showlegend,
+            spanmode = 'hard'
         ))
         fig.add_trace(go.Violin(
             x=exp2_net['proc_binned'][exp2_net['proc_binned'] == label],
@@ -86,7 +87,8 @@ def violin_cmp_2_exp_wait_v_node_count(
             name=exp_2_name,
             side='positive',
             line_color='orange',
-            showlegend=showlegend
+            showlegend=showlegend,
+            spanmode = 'hard'
         ))
         showlegend=False
         
@@ -98,7 +100,8 @@ def violin_cmp_2_exp_wait_v_node_count(
         name=exp_1_name,
         side='negative',
         line_color='blue',
-        showlegend=showlegend
+        showlegend=showlegend,
+        spanmode = 'hard'
     ))
     fig.add_trace(go.Violin(
         x=exp2_cpy['proc_binned'][exp2_cpy['proc_binned'] == 'Overall'],
@@ -108,7 +111,8 @@ def violin_cmp_2_exp_wait_v_node_count(
         name=exp_2_name,
         side='positive',
         line_color='orange',
-        showlegend=showlegend
+        showlegend=showlegend,
+        spanmode = 'hard'
     ))
 
     fig.update_traces(
@@ -195,7 +199,8 @@ def violin_cmp_2_exp_wait_v_walltime(
             name=exp_1_name,
             side='negative',
             line_color='blue',
-            showlegend=False
+            showlegend=False,
+            spanmode = 'hard'
         ))
         fig.add_trace(go.Violin(
             x=exp2_net['walltime_binned'][exp2_net['walltime_binned'] == label],
@@ -205,7 +210,8 @@ def violin_cmp_2_exp_wait_v_walltime(
             name=exp_2_name,
             side='positive',
             line_color='orange',
-            showlegend=False
+            showlegend=False,
+            spanmode = 'hard'
         ))
         
     fig.add_trace(go.Violin(
@@ -216,7 +222,8 @@ def violin_cmp_2_exp_wait_v_walltime(
         name=exp_1_name,
         side='negative',
         line_color='blue',
-        showlegend=True
+        showlegend=True,
+        spanmode = 'hard'
     ))
     fig.add_trace(go.Violin(
         x=exp2_cpy['walltime_binned'][exp2_cpy['walltime_binned'] == 'Overall'],
@@ -226,7 +233,8 @@ def violin_cmp_2_exp_wait_v_walltime(
         name=exp_2_name,
         side='positive',
         line_color='orange',
-        showlegend=True
+        showlegend=True,
+        spanmode = 'hard'
     ))
 
     fig.update_traces(
@@ -314,7 +322,8 @@ def violin_cmp_2_exp_boslo_v_node_count(
             name=exp_1_name,
             side='negative',
             line_color='blue',
-            showlegend=showlegend
+            showlegend=showlegend,
+            spanmode = 'hard'
         ))
         fig.add_trace(go.Violin(
             x=exp2_net['proc_binned'][exp2_net['proc_binned'] == label],
@@ -324,7 +333,8 @@ def violin_cmp_2_exp_boslo_v_node_count(
             name=exp_2_name,
             side='positive',
             line_color='orange',
-            showlegend=showlegend
+            showlegend=showlegend,
+            spanmode = 'hard'
         ))
         showlegend=False
         
@@ -336,7 +346,8 @@ def violin_cmp_2_exp_boslo_v_node_count(
         name=exp_1_name,
         side='negative',
         line_color='blue',
-        showlegend=showlegend
+        showlegend=showlegend,
+        spanmode = 'hard'
     ))
     fig.add_trace(go.Violin(
         x=exp2_cpy['proc_binned'][exp2_cpy['proc_binned'] == 'Overall'],
@@ -346,7 +357,8 @@ def violin_cmp_2_exp_boslo_v_node_count(
         name=exp_2_name,
         side='positive',
         line_color='orange',
-        showlegend=showlegend
+        showlegend=showlegend,
+        spanmode = 'hard'
     ))
 
     fig.update_traces(
@@ -434,7 +446,8 @@ def violin_cmp_2_exp_boslo_v_walltime(
             name=exp_1_name,
             side='negative',
             line_color='blue',
-            showlegend=False
+            showlegend=False,
+            spanmode = 'hard'
         ))
         fig.add_trace(go.Violin(
             x=exp2_net['walltime_binned'][exp2_net['walltime_binned'] == label],
@@ -444,7 +457,8 @@ def violin_cmp_2_exp_boslo_v_walltime(
             name=exp_2_name,
             side='positive',
             line_color='orange',
-            showlegend=False
+            showlegend=False,
+            spanmode = 'hard'
         ))
         # showlegend=False
         
@@ -456,7 +470,8 @@ def violin_cmp_2_exp_boslo_v_walltime(
         name=exp_1_name,
         side='negative',
         line_color='blue',
-        showlegend=True
+        showlegend=True,
+        spanmode = 'hard'
     ))
     fig.add_trace(go.Violin(
         x=exp2_cpy['walltime_binned'][exp2_cpy['walltime_binned'] == 'Overall'],
@@ -466,7 +481,8 @@ def violin_cmp_2_exp_boslo_v_walltime(
         name=exp_2_name,
         side='positive',
         line_color='orange',
-        showlegend=True
+        showlegend=True,
+        spanmode = 'hard'
     ))
 
     fig.update_traces(
@@ -500,4 +516,297 @@ TODO: ADD description
         dcc.Graph(
                 figure = fig
             ),
+    ]
+
+def violin_cmp_2_exp_boslo_v_walltime_95(
+        exp1c1,
+        exp1c2,
+        exp2c1,
+        exp2c2,
+        exp_1_name,
+        exp_2_name,
+):
+
+    c1 = read_rst(exp1c1)
+    c2 = read_rst(exp1c2)
+    c3 = read_rst(exp2c1)
+    c4 = read_rst(exp2c2)
+
+    fig = go.Figure()
+
+    exp1_net = pd.concat([c1, c2], axis=0)
+    exp2_net = pd.concat([c3, c4], axis=0)
+
+    # Remove first 1000 and last 1000 jobs
+    # Cluster warmup period
+    exp1_net = exp1_net.sort_values(by='id')
+    exp2_net = exp1_net.sort_values(by='id')  # Fixed typo here, should be exp2_net
+    exp1_net = exp1_net.iloc[1000:-1000]
+    exp2_net = exp2_net.iloc[1000:-1000]
+
+    exp1_net['walltime'] = exp1_net['walltime'] / 60
+    exp2_net['walltime'] = exp2_net['walltime'] / 60
+
+    exp1_net['bounded_slowdown'] = cal_boslow(exp1_net)
+    exp2_net['bounded_slowdown'] = cal_boslow(exp2_net)
+
+    bins = [0, 10, 30, 60, 120, 250, 500, 1000, 1500, float('inf')]
+    labels = ['(0, 10]', '(10, 30]', '(30, 60]', '(60, 120]', '(120, 250]', '(250, 500]', '(500, 1000]', '(1000, 1500]',
+              '(1500, max]']
+    for _df, name in zip([exp1_net, exp2_net], [exp_1_name, exp_2_name]):
+        _df['walltime_binned'] = pd.cut(_df['walltime'], bins=bins, labels=labels, right=True)
+
+    exp1_cpy = exp1_net.copy()
+    exp2_cpy = exp2_net.copy()
+
+    exp1_cpy['walltime_binned'] = 'Overall'
+    exp2_cpy['walltime_binned'] = 'Overall'
+
+    fig = go.Figure()
+    for label in labels:
+        # Calculate 95% confidence interval for each bin
+        exp1_bin = exp1_net[exp1_net['walltime_binned'] == label]['bounded_slowdown']
+        exp2_bin = exp2_net[exp2_net['walltime_binned'] == label]['bounded_slowdown']
+        
+        exp1_ci = exp1_bin.quantile([0.025, 0.975])
+        exp2_ci = exp2_bin.quantile([0.025, 0.975])
+
+        # Filter data within the confidence interval
+        exp1_filtered = exp1_bin[(exp1_bin >= exp1_ci.iloc[0]) & (exp1_bin <= exp1_ci.iloc[1])]
+        exp2_filtered = exp2_bin[(exp2_bin >= exp2_ci.iloc[0]) & (exp2_bin <= exp2_ci.iloc[1])]
+
+        fig.add_trace(go.Violin(
+            x=exp1_net['walltime_binned'][exp1_net['walltime_binned'] == label],
+            y=exp1_filtered,  # Use filtered data
+            legendgroup=exp_1_name,
+            scalegroup=label,
+            name=exp_1_name,
+            side='negative',
+            line_color='blue',
+            showlegend=False,
+            spanmode = 'hard'
+        ))
+        fig.add_trace(go.Violin(
+            x=exp2_net['walltime_binned'][exp2_net['walltime_binned'] == label],
+            y=exp2_filtered,  # Use filtered data
+            legendgroup=exp_2_name,
+            scalegroup=label,
+            name=exp_2_name,
+            side='positive',
+            line_color='orange',
+            showlegend=False,
+            spanmode = 'hard'
+        ))
+
+    # Calculate 95% confidence interval for overall data
+    exp1_overall_ci = exp1_cpy['bounded_slowdown'].quantile([0.025, 0.975])
+    exp2_overall_ci = exp2_cpy['bounded_slowdown'].quantile([0.025, 0.975])
+
+    # Filter overall data within the confidence interval
+    exp1_overall_filtered = exp1_cpy['bounded_slowdown'][
+        (exp1_cpy['bounded_slowdown'] >= exp1_overall_ci.iloc[0]) & (exp1_cpy['bounded_slowdown'] <= exp1_overall_ci.iloc[1])
+    ]
+    exp2_overall_filtered = exp2_cpy['bounded_slowdown'][
+        (exp2_cpy['bounded_slowdown'] >= exp2_overall_ci.iloc[0]) & (exp2_cpy['bounded_slowdown'] <= exp2_overall_ci.iloc[1])
+    ]
+
+    fig.add_trace(go.Violin(
+        x=exp1_cpy['walltime_binned'][exp1_cpy['walltime_binned'] == 'Overall'],
+        y=exp1_overall_filtered,  # Use filtered data
+        legendgroup=exp_1_name,
+        scalegroup='Overall',
+        name=exp_1_name,
+        side='negative',
+        line_color='blue',
+        showlegend=True,
+        spanmode = 'hard'
+    ))
+    fig.add_trace(go.Violin(
+        x=exp2_cpy['walltime_binned'][exp2_cpy['walltime_binned'] == 'Overall'],
+        y=exp2_overall_filtered,  # Use filtered data
+        legendgroup=exp_2_name,
+        scalegroup='Overall',
+        name=exp_2_name,
+        side='positive',
+        line_color='orange',
+        showlegend=True,
+        spanmode = 'hard'
+    ))
+
+    fig.update_traces(
+        meanline_visible=True,
+        points=False,
+        jitter=0.05,
+        scalemode='count'
+    )
+    fig.update_layout(
+        violingap=0,
+        violingroupgap=0,
+        violinmode='overlay',
+        title="Violin Plot of Bounded Slowdown Distribution by Job Size and Experiment",
+        xaxis_title="Job Walltime (min)",
+        yaxis_title="Bounded Slowdown",
+        legend=dict(
+            yanchor="top",
+            y=0.99,
+            xanchor="left",
+            x=0.01
+        )
+    )
+
+    return [
+        dcc.Markdown(
+            f"""
+TODO: ADD description
+"""
+        ),
+        dcc.Graph(
+            figure=fig
+        ),
+    ]
+
+
+
+def violin_cmp_2_exp_boslo_v_node_count_95(
+        exp1c1,
+        exp1c2,
+        exp2c1,
+        exp2c2,
+        exp_1_name,
+        exp_2_name,
+):
+    c1 = read_rst(exp1c1)
+    c2 = read_rst(exp1c2)
+    c3 = read_rst(exp2c1)
+    c4 = read_rst(exp2c2)
+
+    fig = go.Figure()
+
+    exp1_net = pd.concat([c1, c2], axis=0)
+    exp2_net = pd.concat([c3, c4], axis=0)
+
+    # Remove first 1000 and last 1000 jobs
+    # Cluster warmup period
+    exp1_net = exp1_net.sort_values(by='id')
+    exp2_net = exp2_net.sort_values(by='id')  # Fixed typo here, should be exp2_net
+    exp1_net = exp1_net.iloc[1000:-1000]
+    exp2_net = exp2_net.iloc[1000:-1000]
+
+    exp1_net['bounded_slowdown'] = cal_boslow(exp1_net)
+    exp2_net['bounded_slowdown'] = cal_boslow(exp2_net)
+
+    bins = [0, 128, 256, 512, 1024, 2048, float('inf')]
+    labels = ['(0, 128]', '(128, 256]', '(256, 512]', '(512, 1024]', '(1024, 2048]', '(2048, 2180]']
+    for _df, name in zip([exp1_net, exp2_net], [exp_1_name, exp_2_name]):
+        _df['proc_binned'] = pd.cut(_df['proc1'], bins=bins, labels=labels, right=True)
+
+    exp1_cpy = exp1_net.copy()
+    exp2_cpy = exp2_net.copy()
+
+    exp1_cpy['proc_binned'] = 'Overall'
+    exp2_cpy['proc_binned'] = 'Overall'
+
+    showlegend = True
+    fig = go.Figure()
+    for label in labels:
+        # Calculate 95% confidence interval for each bin
+        exp1_bin = exp1_net[exp1_net['proc_binned'] == label]['bounded_slowdown']
+        exp2_bin = exp2_net[exp2_net['proc_binned'] == label]['bounded_slowdown']
+
+        exp1_ci = exp1_bin.quantile([0.025, 0.975])
+        exp2_ci = exp2_bin.quantile([0.025, 0.975])
+
+        # Filter data within the confidence interval
+        exp1_filtered = exp1_bin[(exp1_bin >= exp1_ci.iloc[0]) & (exp1_bin <= exp1_ci.iloc[1])]
+        exp2_filtered = exp2_bin[(exp2_bin >= exp2_ci.iloc[0]) & (exp2_bin <= exp2_ci.iloc[1])]
+
+        fig.add_trace(go.Violin(
+            x=exp1_net['proc_binned'][exp1_net['proc_binned'] == label],
+            y=exp1_filtered,  # Use filtered data
+            legendgroup=exp_1_name,
+            scalegroup=label,
+            name=exp_1_name,
+            side='negative',
+            line_color='blue',
+            showlegend=showlegend,
+            spanmode = 'hard'
+        ))
+        fig.add_trace(go.Violin(
+            x=exp2_net['proc_binned'][exp2_net['proc_binned'] == label],
+            y=exp2_filtered,  # Use filtered data
+            legendgroup=exp_2_name,
+            scalegroup=label,
+            name=exp_2_name,
+            side='positive',
+            line_color='orange',
+            showlegend=showlegend,
+            spanmode = 'hard'
+        ))
+        showlegend = False
+
+    # Calculate 95% confidence interval for overall data
+    exp1_overall_ci = exp1_cpy['bounded_slowdown'].quantile([0.025, 0.975])
+    exp2_overall_ci = exp2_cpy['bounded_slowdown'].quantile([0.025, 0.975])
+
+    # Filter overall data within the confidence interval
+    exp1_overall_filtered = exp1_cpy['bounded_slowdown'][
+        (exp1_cpy['bounded_slowdown'] >= exp1_overall_ci.iloc[0]) & (exp1_cpy['bounded_slowdown'] <= exp1_overall_ci.iloc[1])
+    ]
+    exp2_overall_filtered = exp2_cpy['bounded_slowdown'][
+        (exp2_cpy['bounded_slowdown'] >= exp2_overall_ci.iloc[0]) & (exp2_cpy['bounded_slowdown'] <= exp2_overall_ci.iloc[1])
+    ]
+
+    fig.add_trace(go.Violin(
+        x=exp1_cpy['proc_binned'][exp1_cpy['proc_binned'] == 'Overall'],
+        y=exp1_overall_filtered,  # Use filtered data
+        legendgroup=exp_1_name,
+        scalegroup='Overall',
+        name=exp_1_name,
+        side='negative',
+        line_color='blue',
+        showlegend=showlegend,
+            spanmode = 'hard'
+    ))
+    fig.add_trace(go.Violin(
+        x=exp2_cpy['proc_binned'][exp2_cpy['proc_binned'] == 'Overall'],
+        y=exp2_overall_filtered,  # Use filtered data
+        legendgroup=exp_2_name,
+        scalegroup='Overall',
+        name=exp_2_name,
+        side='positive',
+        line_color='orange',
+        showlegend=showlegend,
+            spanmode = 'hard'
+    ))
+
+    fig.update_traces(
+        meanline_visible=True,
+        points=False,
+        jitter=0.05,
+        scalemode='count'
+    )
+    fig.update_layout(
+        violingap=0,
+        violingroupgap=0,
+        violinmode='overlay',
+        title="Violin Plot of Bounded Slowdown Distribution by Job Size and Experiment",
+        xaxis_title="Job Size",
+        yaxis_title="Bounded Slowdown",
+        legend=dict(
+            yanchor="top",
+            y=0.99,
+            xanchor="left",
+            x=0.01
+        )
+    )
+
+    return [
+        dcc.Markdown(
+            f"""
+TODO: ADD description
+"""
+        ),
+        dcc.Graph(
+            figure=fig
+        ),
     ]
